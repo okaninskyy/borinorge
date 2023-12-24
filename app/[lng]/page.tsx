2d5d20726d8c93927c462b82ab2d82421ce8ef8d
Vadym Kaninskyi <vadym.kaninskyi@gmail.com>
2023-12-24 15:34:12 +0000

    Merged in BOR-1-multilang (pull request #1)
    BOR-1 multilang
    
    * BOR-1: add i18n
    
    * BOR-1: add language switcher
    
    * BOR-1: add documentation

---
+++ app/[lng]/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import { useTranslation } from '../i18n'
+import { Footer } from './components/footer'
+
+export default async function Page({ params: { lng } }: { params: { lng: string } }) {
+  const { t } = await useTranslation(lng)
+
+  return (
+    <>
+      <h1>{t('title')}</h1>
+      <Link href={`/${lng}/second-page`}>
+        {t('to-second-page')}
+      </Link>
+      <Footer lng={lng}/>
+    </>
+  )
+}
